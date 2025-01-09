99e58358e9f1647f2cebc15e5fe4349ade5deb3e
Morten Hersson <mhersson@gmail.com>
2025-01-09 19:02:27 +0100

    Ensure Proper Initialization of Mermaid Library
    Wrap WebSocket logic in a DOMContentLoaded event listener to guarantee
    that the Mermaid library is fully loaded before invoking renderMermaid.

--- internal/previewserver/web/ws.js
+++ internal/previewserver/web/ws.js
@@ -1,74 +1,72 @@
-const ws = new WebSocket("ws://localhost:%d/ws");
+document.addEventListener("DOMContentLoaded", () => {
+  const ws = new WebSocket("ws://localhost:%d/ws");
 
-let debounceTimeout;
+  let debounceTimeout;
 
-function debounce(func, delay) {
-  return function (...args) {
-    clearTimeout(debounceTimeout);
-    debounceTimeout = setTimeout(() => func.apply(this, args), delay);
-  };
-}
-
-const updateTitle = debounce((title) => {
-  document.title = title;
-}, 200);
-
-const renderMermaid = async () => {
-  if (typeof window.mermaid !== "undefined") {
-    await window.mermaid.run({
-      querySelector: ".language-mermaid",
-    });
-  } else {
-    console.log("Mermaid is not defined.");
+  function debounce(func, delay) {
+    return function (...args) {
+      clearTimeout(debounceTimeout);
+      debounceTimeout = setTimeout(() => func.apply(this, args), delay);
+    };
   }
-};
 
-ws.onopen = function (event) {
-  console.log("WebSocket connection established");
-};
+  const updateTitle = debounce((title) => {
+    document.title = title;
+  }, 200);
 
-ws.onmessage = function (event) {
-  console.log("Got WebSocket message");
-
-  const response = JSON.parse(event.data);
-  const renderedHtml = response.HTML;
-  const section = response.Section;
-  const title = "mpls - " + response.Title;
-
-  if (title != document.title) {
-    updateTitle(title);
-  }
-  //console.log(renderedHtml);
-
-  document.body.innerHTML = `<div id="content">${renderedHtml}</div>`;
-
-  renderMermaid();
-
-  if (section) {
-    const targetElement = document.querySelector(`#${section}`);
-
-    if (targetElement) {
-      targetElement.scrollIntoView({ behavior: "smooth", block: "start" });
-
-      const offset = 50;
-      const elementRect = targetElement.getBoundingClientRect();
-      const elementTop = elementRect.top + window.scrollY;
-
-      window.scrollTo({
-        top: elementTop - offset,
-        behavior: "smooth",
+  const renderMermaid = async () => {
+    const mermaidElements = document.querySelectorAll(".language-mermaid");
+    if (mermaidElements.length > 0) {
+      await window.mermaid.run({
+        querySelector: ".language-mermaid",
       });
-    } else {
-      console.warn("Target element not found for cursor position:", section);
     }
-  }
-};
+  };
 
-ws.onerror = function (event) {
-  console.error("WebSocket error observed:", event);
-};
+  ws.onopen = function (event) {
+    console.log("WebSocket connection established");
+  };
 
-ws.onclose = function (event) {
-  window.close();
-  console.log("WebSocket connection closed:", event);
-};
+  ws.onmessage = function (event) {
+    console.log("Got WebSocket message");
+
+    const response = JSON.parse(event.data);
+    const renderedHtml = response.HTML;
+    const section = response.Section;
+    const title = "mpls - " + response.Title;
+
+    if (title !== document.title) {
+      updateTitle(title);
+    }
+
+    // console.log(renderedHtml);
+
+    document.body.innerHTML = `<div id="content">${renderedHtml}</div>`;
+
+    renderMermaid();
+
+    if (section) {
+      const targetElement = document.querySelector(`#${section}`);
+
+      if (targetElement) {
+        targetElement.scrollIntoView({ behavior: "smooth", block: "start" });
+
+        const offset = 50;
+        const elementRect = targetElement.getBoundingClientRect();
+        const elementTop = elementRect.top + window.scrollY;
+
+        window.scrollTo({
+          top: elementTop - offset,
+          behavior: "smooth",
+        });
+      } else {
+        console.warn("Target element not found for cursor position:", section);
+      }
+    }
+  };
+
+  ws.onclose = function (event) {
+    window.close();
+    console.log("WebSocket connection closed:", event);
+  };
+});
